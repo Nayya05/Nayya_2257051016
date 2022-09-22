a3f45d12a29e99b603dab1bf93aeb7bb7060c9e3
Nayya05 <112569018+Nayya05@users.noreply.github.com>
2022-09-22 12:53:52 +0700

    Add files via upload

---
+++ 2257051016_NayyaTharissa_praktest2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+	char angka = 2;
+	cin >> angka;
+	switch (angka){
+	case '1' :
+		cout << "Nilai anda satu " << endl;
+		break;
+	case '2':
+		cout << "Nilai anda dua" << endl;
+		break;
+	case '3':
+		cout << "Nilai anda tiga" << endl;
+		break;
+		default :
+			cout << " Nilai lebih dari tiga " << endl;
+			
+			return 0;
+				
+			
+			}
+	
+	}
+	
+	
