5b25719199285efd7802ac829e6b3ae66e71c83f
Nayya05 <112569018+Nayya05@users.noreply.github.com>
2022-09-29 13:17:29 +0700

    Add files via upload

---
+++ NayyaTharissa_2257051016_praktest3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main (){
+	int g;
+	int b;
+	
+	cin >> g;
+	cin >> b;
+		
+	for (int u = g; u<=b; u+=u){
+ 	cout << u << endl;
+	}
+		
+	
+	return 0;
+}
