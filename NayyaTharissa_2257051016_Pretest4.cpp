76bcebbbe94a6323cd7a09b35787d89ee01a925e
Nayya05 <112569018+Nayya05@users.noreply.github.com>
2022-10-13 11:59:57 +0700

    Add files via upload

---
+++ NayyaTharissa_2257051016_Pretest4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int beratbadan , tinggibadan ;{
+
+	cin >> berat badan , tinggi badan ;
+	if (berat badan , tinggi badan )%18,5>=0&&berat badan ,tinggi badan (m)%25,0==0){
+		cout << "Normal ";
+	}else if ( berat badan  tinggi badan %17,0>==0){
+		cout << "Kurus, kekurangan berat badan tingkat berat" ;
+	}else if (berat badan ( kg),tinggi badan ( m) %17,0>=0&&bert badan (kg),tinggi badan (m)%18,4==0){
+		cout << "Kurus, kekurangan berat badan ringan";
+	}else if (berat badan (kg),tinggi badan (m)
+	
+	return 0;
+}
+	          
+			}	} 
