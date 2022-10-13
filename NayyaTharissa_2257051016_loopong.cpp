d0ee51ce699a6f39857a31264dbe482ddf0faa84
Nayya05 <112569018+Nayya05@users.noreply.github.com>
2022-10-13 11:58:48 +0700

    Add files via upload

---
+++ NayyaTharissa_2257051016_loopong.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main (){
+	for (int i = 20;i > 0; i--){
+		for (int a=0;a<i; a++){
+			cout << " #";
+		}
+		cout << endl;
+		
+	}
+	
+	int b =0;
+	while (b < 20){
+		cout << "Perulangan ke : "<< b <<endl;
+		b++;
+	
+	}
+	
+	
+	int a = 20;
+	do{
+		cout << "Perulangan ke : " << endl;
+		a++;
+	}while (a < 30 );
+	
+	
+}
+
+
+
+
