f1f94ee31b115d2af0775e8ae3455e2f9c6aba3c
Nayya05 <112569018+Nayya05@users.noreply.github.com>
2023-04-12 23:20:15 +0700

    Add files via upload

---
+++ struct.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+struct nilai{
+	double mtk;
+	double bindo;
+	double bing;
+	double ipa;
+	
+	
+};
+
+int main (){
+	nilai bahtiar;
+	bahtiar.mtk = 90;
+	bahtiar.bindo = 80;
+	bahtiar.ipa= 95;
+	bahtiar.bing = 100;
+	cout <<"Nilai Mtk : " << bahtiar.mtk<< endl;
+	cout << " Nilai Bindo : " << bahtiar.bindo << endl;
+	cout << " Nilai ipa : " << bahtiar.ipa << endl;
+	cout << " Nilai Bing : " << bahtiar.bing << endl;
+	
+}
